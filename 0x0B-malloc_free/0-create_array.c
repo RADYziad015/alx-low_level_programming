5200c45ece9ad38b401a7c678bf705c6e41a14be
radyziad <ziad@rady.com>
2024-01-22 11:01:54 -0800

    :v

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,7 +10,7 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *n = malloc(size);
+	char *n = malloc(sizeof(char));
 
 	if (size == 0 || n == 0)
 		return (0);
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @owner: owner's name
+ *
+ * Description: just along dog struct in a big kitty world
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner
+};
+
+#endif
